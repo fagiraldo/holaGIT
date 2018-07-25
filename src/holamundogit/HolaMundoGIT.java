0731b729639e3f8f83c125803a7625d46a0a8e1f
Labing I5 <Labing I5@LabingI5-0-101>
2018-07-25 11:27:44 -0500

    Commit inicial Hola Mundo Git 2

--- src/holamundogit/HolaMundoGIT.java
+++ src/holamundogit/HolaMundoGIT.java
@@ -19,6 +19,7 @@ public class HolaMundoGIT {
      */
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("Nueva instruccion");
     }
     
 }
