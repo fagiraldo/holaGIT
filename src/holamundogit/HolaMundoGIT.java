9339c6d4b91a6f6362718ea78285600d15ed9f0d
Labing I5 <Labing I5@LabingI5-0-101>
2018-07-25 11:20:08 -0500

    Commit inicial Hola Mundo Git

---
+++ src/holamundogit/HolaMundoGIT.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundogit;
+
+/**
+ *
+ * @author Labing I5
+ * @version  0.1
+ * Ejemplo Gestion proyecto con GIT - GITHUB
+ */
+public class HolaMundoGIT {
+
+    /**
+     * Main del Program
+     * @param args Argumentos linea de comandos.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
